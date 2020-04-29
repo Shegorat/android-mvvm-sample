4d7b8e9d73875f1032d11ef5af83a4b2d018ad5b
Shegorat <Shegorat007@gmail.com>
2020-04-29 22:24:47 +0600

    Add live data extension methods

--- app/src/main/java/com/shegorat/mvvm/ext/LiveDataExtension.kt
+++ app/src/main/java/com/shegorat/mvvm/ext/LiveDataExtension.kt
@@ -1,2 +1,108 @@
 package com.shegorat.mvvm.ext
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+
+// Nullable
+fun <T, R> LiveData<T?>.map(mapper: (T?) -> R?): LiveData<R?> {
+    return MediatorLiveData<R?>().apply {
+        addSource(this@map) {
+            value = mapper(it)
+        }
+    }
+}
+
+fun <T> LiveData<T?>.filter(predicate: (T?) -> Boolean): LiveData<T?> {
+    return MediatorLiveData<T?>().apply {
+        addSource(this@filter) {
+            if (predicate(it))
+                value = it
+        }
+    }
+}
+
+fun <T> LiveData<T?>.distinct(): LiveData<T?> {
+    return MediatorLiveData<T?>().apply {
+        addSource(this@distinct) {
+            if (value != it)
+                value = it
+        }
+    }
+}
+
+fun <T, T2> LiveData<T?>.trigger(trigger: LiveData<T2?>, action: (T?, T2?) -> T): LiveData<T?> {
+    return MediatorLiveData<T>().apply {
+        var a1: T? = null
+        var a2: T2? = null
+
+        fun update() {
+            val v = action(a1, a2)
+            if (v != value)
+                value = v
+        }
+
+        addSource(this@trigger) {
+            a1 = it
+            update()
+        }
+
+        addSource(trigger) {
+            a2 = it
+            update()
+        }
+    }
+}
+
+fun <T1, T2, R> LiveData<T1?>.zip(src2: LiveData<T2?>, zipper: (T1?, T2?) -> R?): LiveData<R?> {
+    return MediatorLiveData<R?>().apply {
+        var a1: T1? = null
+        var a2: T2? = null
+
+        fun update() {
+            value = zipper(a1, a2)
+        }
+
+        addSource(this@zip) {
+            a1 = it
+            update()
+        }
+
+        addSource(src2) {
+            a2 = it
+            update()
+        }
+    }
+}
+
+fun <T1, T2, R> LiveData<T1?>.combine(
+    src2: LiveData<T2?>,
+    combiner: (T1?, T2?) -> R?
+): LiveData<R?> {
+    return MediatorLiveData<R?>().apply {
+        var a1: T1? = null
+        var a2: T2? = null
+
+        fun update() {
+            value = combiner(a1, a2)
+        }
+
+        addSource(this@combine) {
+            a1 = it
+            update()
+        }
+
+        addSource(src2) {
+            a2 = it
+            update()
+        }
+    }
+}
+
+fun <T> LiveData<T?>.notNull(): LiveData<T> {
+    return MediatorLiveData<T>().apply {
+        addSource(this@notNull) {
+            if (it != null)
+                value = it
+        }
+    }
+}
--- app/src/main/java/com/shegorat/mvvm/http/RequestScope.kt
+++ app/src/main/java/com/shegorat/mvvm/http/RequestScope.kt
@@ -1,6 +1,23 @@
 package com.shegorat.mvvm.http
 
-interface RequestScope {
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
+interface RequestScope : CoroutineScope {
 
+    val apiJob: Job
+    override val coroutineContext: CoroutineContext
+        get() = apiJob + Dispatchers.Main
+}
+
+fun RequestScope.api() = this.launch {
+
+}
+
+suspend fun <T> RequestScope.io(block: suspend () -> T) = withContext(Dispatchers.IO) {
+    block()
+}
+
+suspend fun <T> RequestScope.main(block: suspend () -> T) = withContext(Dispatchers.Main) {
+    block
 }
